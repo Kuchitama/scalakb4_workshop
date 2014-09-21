dd95617e80ec87099a9ea1e947db423a0b1c1e0a
kuchitama <kiyo.kunihira@gmail.com>
2014-09-21 16:38:06 +0900

    ちょっとしたテストを追加

--- src/test/scala/NameSelectorSpec.scala
+++ src/test/scala/NameSelectorSpec.scala
@@ -1,8 +1,17 @@
 import org.scalatest._
 import org.scalatest.matchers.ShouldMatchers
 
+import com.example.NameSelector
+
 class NameSelectorSpec extends FlatSpec with ShouldMatchers {
-  "Hello" should "have tests" in {
-    true should be === true
+
+  // NameSelector#names に参加者の名前が含まれることを試験
+  "names" should "contains names of participants" in {
+    val names = NameSelector.names
+    
+    // 1件以上の名前があること
+    names.size should be > 0
+    // 名前が含まれていること
+    names should contain("aa7th")
   }
 }
