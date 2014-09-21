173a1cfe291913b0d1e32f4d4b48e5d75b0f1cbe
kuchitama <kiyo.kunihira@gmail.com>
2014-09-21 16:27:58 +0900

    names.txtから名前を読み込んで出力する

--- src/main/scala/com/example/NameSelector.scala
+++ src/main/scala/com/example/NameSelector.scala
@@ -1,9 +1,11 @@
 package com.example
 
 import scala.util.Random
+import scalax.file.Path
 
 object NameSelector {
-  lazy val names = List("aa7th", "ryu1_okd", "mumoshu")
+  lazy val namesFile:Path = Path("names.txt")
+  lazy val names:List[String] = namesFile.lines().toList
 
   def main(args: Array[String]): Unit = {
     val result = Random.shuffle(names).head
